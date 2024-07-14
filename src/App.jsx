65342ae966470c9d00284e40ac1c3afce013b50a
izzy010101 <165940159+izzy010101@users.noreply.github.com>
2024-07-14 03:42:20 -0500

    custom tip calculation made along with some tiny changes in handlers

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,27 @@ function App() {
   const [mainBill, setMainBill] = useState(0);
   const [numOfPpl, setNumOfPpl] = useState(1); //ovde stavljamo koji je default po osobi, stavio sam 1 a moze i 0 da se stavi...
   const [tipPercentage, setTipPercentage] = useState(0);
+  //dodajem custom tip state
+  const [customTip, setCustomTip] = useState('');
 
   //pravimo event za input field
   const handleBillChange = (e) => setMainBill(e.target.value);
   const handlePeopleChange = (e) => setNumOfPpl(e.target.value);
+  
 
   //ovo updatuje state "tipPercentage" sa onim buttonom koji smo kliknuli dole
   const handleTipPercentage = (percentage) => {
     setTipPercentage(percentage);
+    setCustomTip(''); //clear custom ako smo izabrali procenat koji je ponudjen
   };
 
   //matematicka logika za racunanje procenta i na dve decimale se prikazuje
   const calculateTipAmount = () => {
-    return (mainBill * (tipPercentage / 100)).toFixed(2);
+    const bill = parseFloat(mainBill);
+    const tip = customTip ? parseFloat(customTip) : tipPercentage;
+    return (bill * (tip / 100) || 0).toFixed(2);
   };
+
 
   //ovde se racuna bill per person i dodaje tip
   const billPerPerson = () => {
@@ -30,14 +37,15 @@ function App() {
       return '0.00';
     }
 
-    const tipAmount = calculateTipAmount(); 
-    const totalBill = bill + parseFloat(tipAmount);
+    const tipAmount = parseFloat(calculateTipAmount()); 
+    const totalBill = bill + tipAmount;
     return (totalBill / people).toFixed(2);
   };
 
-  const totalBillWithTip = () => {
-    const bill = parseFloat(mainBill);
-    const tipAmount = parseFloat(calculateTipAmount());
+  //total bill with tip, sending 2 params which we use down when calling the function
+  const totalBillWithTip = (billToPass, calculateTip) => {
+    const bill = parseFloat(billToPass);
+    const tipAmount = parseFloat(calculateTip);
 
     if (isNaN(bill) || bill <= 0) {
       return '0.00';
@@ -47,6 +55,23 @@ function App() {
     return total.toFixed(2);
   };
 
+  //handler za custom tip amount , uzimam value inputa i regex stavljam da moze samo broj jer je input type text, setujem custom tip i praznim ako smo izabrali vec procenat
+  const handleCustomTip = (e) => {
+    const value = e.target.value;
+    if (/^\d*\.?\d*$/.test(value)) {
+      setCustomTip(value);
+      setTipPercentage(0); // Clear preset percentage if custom tip is entered
+    }
+  }
+
+
+  // Handle Enter key press and perform a calculation
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      totalBillWithTip(mainBill, calculateTipAmount());
+      
+    }
+  };
 
 
   return (
@@ -73,8 +98,14 @@ function App() {
             <button className=' bg-[#01464c] h-[30px] w-[75px] rounded-md mx-1 my-1 text-white' onClick={() => handleTipPercentage(15)}>15%</button>
             <button className=' bg-[#01464c] h-[30px] w-[75px] rounded-md mx-1 my-1 text-white' onClick={() => handleTipPercentage(25)}>25%</button>
             <button className=' bg-[#01464c] h-[30px] w-[75px] rounded-md mx-1 my-1 text-white' onClick={() => handleTipPercentage(50)}>50%</button>
-            <input type="text" className='h-[30px] w-[75px] ml-[5px] pl-[10px] font-mono placeholder-[#01464c] text-md bg-[#f4f7fa] rounded-md' placeholder='Custom' />
-            {/* OVAJ CUSTOM INPUT NIJE SREDJEN ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */}
+            <input type="text" 
+                  className='h-[30px] w-[75px] ml-[5px] pl-[10px] font-mono placeholder-[#01464c] text-md bg-[#f4f7fa] rounded-md' 
+                  placeholder='Custom'
+                  value={customTip}
+                  onChange={handleCustomTip}
+                  onKeyDown={handleKeyDown}
+                  />
+            {/* CUSTOM SREDJEN ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */}
           </div>
 
           <div className='mt-6'>
@@ -94,7 +125,7 @@ function App() {
           <div className='mt-2'>
             <p>Tip Amount</p>
             <span>/person</span>
-            <h2>${calculateTipAmount()}</h2>
+            <h2>${(parseFloat(calculateTipAmount()) / numOfPpl).toFixed(2)}</h2>
           </div>
 
 
@@ -107,7 +138,7 @@ function App() {
 
           <div className='mt-20'>
             <h1>Total with tip</h1>
-            <p>${totalBillWithTip()}</p>
+            <p>${totalBillWithTip(mainBill, calculateTipAmount())}</p>
           </div>
 
         </div>
